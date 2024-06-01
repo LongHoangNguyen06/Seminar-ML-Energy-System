2deed668b745a40dc71d30844252abbbc981083d
Long Hoang Nguyen <long.pollehn@gmail.com>
2024-06-01 10:18:10 +0200

    Hide pycache

---
+++ pipeline/io/__init__.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+ROOT_DIR = "/home/long/Desktop/seminar-ml-energy-system/data"
+
+
+def load_data():
+    """
+    Load all data from the data folder.
+
+    Returns
+    -------
+    capacity : pd.DataFrame
+        Installed capacity of power plants in Germany.
+    prices : pd.DataFrame
+        Electricity prices in Europe.
+    supply : pd.DataFrame
+        Realised supply of electricity in Germany.
+    demand : pd.DataFrame
+        Realised demand of electricity in Germany.
+    weather : pd.DataFrame
+        Weather data in Germany.
+    """
+    capacity = pd.read_csv(
+        f"{ROOT_DIR}/Installed_Capacity_Germany.csv",
+        sep=";",
+        thousands=".",
+        decimal=",",
+        parse_dates=["Date from", "Date to"],
+    )
+    prices = pd.read_csv(
+        f"{ROOT_DIR}/Prices_Europe.csv",
+        sep=";",
+        decimal=",",
+        parse_dates=["Date from", "Date to"],
+    )
+    supply = pd.read_csv(
+        f"{ROOT_DIR}/Realised_Supply_Germany.csv",
+        sep=";",
+        thousands=".",
+        decimal=",",
+        parse_dates=["Date from", "Date to"],
+    )
+    demand = pd.read_csv(
+        f"{ROOT_DIR}/Realised_Demand_Germany.csv",
+        sep=";",
+        thousands=".",
+        decimal=",",
+        parse_dates=["Date from", "Date to"],
+    )
+    weather = pd.read_csv(
+        f"{ROOT_DIR}/Weather_Data_Germany.csv",
+        sep=",",
+        decimal=",",
+        parse_dates=["forecast_origin", "time"],
+    )
+    return capacity, prices, supply, demand, weather
