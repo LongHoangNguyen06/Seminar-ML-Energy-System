32b168f160bd33e9859d4fa3b7beb3ca4fa09070
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-06-17 18:13:09 +0200

    Torch's dataloaders

--- pipeline/config.py
+++ pipeline/config.py
@@ -23,7 +23,105 @@ CONF.data.process_raw_data = True
 CONF.data.price_normalization_constant = 10000.0  # euro/MWh
 CONF.data.loaded_raw_data = False  # Don't change this
 CONF.data.normalize_data = True
-CONF.data.plot = True  # False to make faster
+CONF.data.plot = False  # False to make faster
 
 # Model configuration for 1 hour forecasting
+CONF.model.horizon = 24
+CONF.model.batch_size = 32
 CONF.model.supply1.lag = 24  # hours
+CONF.model.supply24.lag = 24 * 7  # hours
+
+# General configuration
+CONF.model.ignore_columns = [
+    "Date from",
+    "Date to",
+    "train",
+    "val",
+    "test",
+    "weather_forecast_origin",
+]
+CONF.model.features = [
+    "prices_Germany/Luxembourg [€/MWh]",
+    "prices_Belgium [€/MWh]",
+    "prices_Denmark 1 [€/MWh]",
+    "prices_Denmark 2 [€/MWh]",
+    "prices_France [€/MWh]",
+    "prices_Netherlands [€/MWh]",
+    "prices_Norway 2 [€/MWh]",
+    "prices_Austria [€/MWh]",
+    "prices_Sweden 4 [€/MWh]",
+    "prices_Switzerland [€/MWh]",
+    "prices_Czech Republic [€/MWh]",
+    "prices_Italy (North) [€/MWh]",
+    "prices_Slovenia [€/MWh]",
+    "capacity_Biomass [MW]",
+    "capacity_Hydro Power [MW]",
+    "capacity_Wind Offshore [MW] ",
+    "capacity_Wind Onshore [MW]",
+    "capacity_Photovoltaic [MW]",
+    "capacity_Other Renewable [MW]",
+    "capacity_Nuclear Power [MW]",
+    "capacity_Lignite [MW]",
+    "capacity_Coal [MW]",
+    "capacity_Natural Gas [MW]",
+    "capacity_Pumped Storage [MW]",
+    "capacity_Other Conventional [MW]",
+    "demand_Total (Grid Load) [MWh]",
+    "demand_Residual Load [MWh]",
+    "demand_Pumped Storage [MWh]",
+    "weather_cdir_min",
+    "weather_cdir_max",
+    "weather_cdir_mean",
+    "weather_z_min",
+    "weather_z_max",
+    "weather_z_mean",
+    "weather_msl_min",
+    "weather_msl_max",
+    "weather_msl_mean",
+    "weather_blh_min",
+    "weather_blh_max",
+    "weather_blh_mean",
+    "weather_tcc_min",
+    "weather_tcc_max",
+    "weather_tcc_mean",
+    "weather_u10_min",
+    "weather_u10_max",
+    "weather_u10_mean",
+    "weather_v10_min",
+    "weather_v10_max",
+    "weather_v10_mean",
+    "weather_t2m_min",
+    "weather_t2m_max",
+    "weather_t2m_mean",
+    "weather_ssr_min",
+    "weather_ssr_max",
+    "weather_ssr_mean",
+    "weather_tsr_min",
+    "weather_tsr_max",
+    "weather_tsr_mean",
+    "weather_sund_min",
+    "weather_sund_max",
+    "weather_sund_mean",
+    "weather_tp_min",
+    "weather_tp_max",
+    "weather_tp_mean",
+    "weather_fsr_min",
+    "weather_fsr_max",
+    "weather_fsr_mean",
+    "weather_u100_min",
+    "weather_u100_max",
+    "weather_u100_mean",
+    "weather_v100_min",
+    "weather_v100_max",
+    "weather_v100_mean",
+]
+CONF.model.targets = [
+    "supply_Biomass [MW]",
+    "supply_Hydro Power [MW]",
+    "supply_Wind Offshore [MW] ",
+    "supply_Wind Onshore [MW]",
+    "supply_Photovoltaic [MW]",
+    "supply_Other Renewable [MW]",
+    "supply_Pumped Storage [MW]",
+]
+CONF.model.device = "cuda"  # cuda or cpu
