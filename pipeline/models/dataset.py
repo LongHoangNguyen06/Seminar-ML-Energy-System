ba97286997d28b7c08bda338c773e20d95d9fbff
Long Nguyen <long.nguyen@student.uni-tuebingen.de>
2024-06-18 12:16:51 +0200

    Belgium

--- pipeline/models/dataset.py
+++ pipeline/models/dataset.py
@@ -4,9 +4,9 @@ from torch.utils.data import Dataset
 
 
 class TimeSeriesDataset(Dataset):
-    def __init__(self, df: pd.DataFrame, hyperparameters):
+    def __init__(self, df: pd.DataFrame, hyperparameters, features=None):
         self.dataframe = df
-        self.features = hyperparameters.model.features
+        self.features = hyperparameters.model.features if features is None else features
         self.targets = hyperparameters.model.targets
         self.lag = hyperparameters.model.lag
         self.horizons = hyperparameters.model.horizons
@@ -31,14 +31,14 @@ class TimeSeriesDataset(Dataset):
             self.dataframe.loc[
                 [idx - lag for lag in range(self.lag)], self.features
             ].values.astype(float)
-        )  # Shape: [n_batches, n_lag, n_features]
+        )  # Shape: [n_lag, n_features]
 
         # Collect lagged targets as additional features
         input_targets = torch.tensor(
             self.dataframe.loc[
                 [idx - lag for lag in range(0, self.lag)], self.targets
             ].values.astype(float)
-        )  # Shape: [n_batches, n_lag, num_targets]
+        )  # Shape: [n_lag, num_targets]
 
         # Concatenate input features and lagged targets along the feature dimension
         inputs = torch.cat((input_features, input_targets), dim=-1)
@@ -48,5 +48,20 @@ class TimeSeriesDataset(Dataset):
             self.dataframe.loc[
                 [idx + horizon for horizon in self.horizons], self.targets
             ].values.astype(float)
-        )  # Shape will be [n_batches, n_horizons, n_outputs]
+        )  # Shape will be [n_horizons, n_outputs]
         return inputs.to(torch.float32), targets.to(torch.float32)
+
+    def get_feature_names(self):
+        # Adjust start index to accommodate the lag
+        rets = []
+        # Collect inputs using the lag
+        for lag in range(self.lag):
+            for feature in self.features:
+                rets.append(f"{feature}_lag_{lag}")
+
+        # Collect lagged targets as additional features
+        for lag in range(self.lag):
+            for target in self.targets:
+                rets.append(f"{target}_lag_{lag}")
+
+        return rets
